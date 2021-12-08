b1c7973e37143bdd4addf5b963bd70ff68617900
Kelvin Alisson Cantarino <kelvininfortec@gmail.com>
2021-12-08 09:24:46 -0300

    fix: correction end-point of tests

--- tests/test_routes_exists.py
+++ tests/test_routes_exists.py
@@ -106,7 +106,7 @@ def teste_route_order_specific_exists(route_matcher):
     try:
         assert route_matcher("/api/order/1")
     except NotFound:
-        fail('Verifique se a rota "api/order<order_id>" existe')
+        fail('Verifique se a rota "api/order/<order_id>" existe')
     except InternalServerError:
         fail('Seu servidor está com erro interno na rota "/api/order", essa rota não é capaz de processar uma requisição')
 
@@ -115,7 +115,7 @@ def teste_route_order_specific_user_exists(route_matcher):
     try:
         assert route_matcher("/api/order/1/user")
     except NotFound:
-        fail('Verifique se a rota "api/order<order_id>/user" existe')
+        fail('Verifique se a rota "api/order/<order_id>/user" existe')
     except InternalServerError:
         fail('Seu servidor está com erro interno na rota "/api/order", essa rota não é capaz de processar uma requisição')
 
@@ -124,6 +124,6 @@ def teste_route_order_specific_user_exists(route_matcher):
     try:
         assert route_matcher("/api/order/1/technicians")
     except NotFound:
-        fail('Verifique se a rota "api/order<order_id>/technicians" existe')
+        fail('Verifique se a rota "api/order/<order_id>/technicians" existe')
     except InternalServerError:
         fail('Seu servidor está com erro interno na rota "/api/order", essa rota não é capaz de processar uma requisição')
