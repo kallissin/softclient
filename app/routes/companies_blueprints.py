00fd70d028ba99a8dfed47f929f3697272e62ca0
Gabriel Cavalcante <gabriel.f.cavalcante@hotmail.com>
2021-12-08 17:33:17 -0300

    feat: company and key models + basic blueprints

--- app/routes/companies_blueprints.py
+++ app/routes/companies_blueprints.py
@@ -1,1 +1,6 @@
-# companies_blueprints
+from flask import Blueprint
+from app.controllers.companies_controllers import get_all
+
+bp = Blueprint('db_companies', __name__, url_prefix='/company')
+
+bp.get("")(get_all)
