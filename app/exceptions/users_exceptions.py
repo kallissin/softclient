4409cf47aaebe425cb4ae6b4774ebea1c1f66a80
Kelvin Alisson Cantarino <kelvininfortec@gmail.com>
2021-12-09 14:08:10 -0300

    feat: create exceptions and completed methods

--- app/exceptions/users_exceptions.py
+++ app/exceptions/users_exceptions.py
@@ -1,1 +1,14 @@
-# users_exceptions
+class InvalidBirthDateError(Exception):
+    ...
+
+class KeyTypeError(Exception):
+    list_keys = ["name", "email", "password", "registration", "role", "company_id"]
+
+    def __init__(self, data: dict):
+        self.message = {
+            "status": "error",
+            "message": [f"{key} is required" for key in self.list_keys if key not in data.keys()]
+        }
+        self.code = 400
+
+        super().__init__(self.message, self.code)
