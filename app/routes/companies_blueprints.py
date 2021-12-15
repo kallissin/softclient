0dd71337a6173cb9e0fd95fc2ec1496c44a47616
Gabriel Cavalcante <gabriel.f.cavalcante@hotmail.com>
2021-12-15 16:09:23 -0300

    feat: get company by trading name.

--- app/routes/companies_blueprints.py
+++ app/routes/companies_blueprints.py
@@ -1,10 +1,11 @@
 from flask import Blueprint
-from app.controllers.companies_controllers import delete_company, get_all, get_company_users, get_one, create_company, login, update_company
+from app.controllers.companies_controllers import delete_company, get__by_name, get_all, get_company_users, get_one, create_company, login, update_company
 
 bp = Blueprint('db_companies', __name__, url_prefix='/company')
 
 bp.get("")(get_all)
 bp.get("/<int:company_id>")(get_one)
+bp.get("/<string:company_name>")(get__by_name)
 bp.get("<int:company_id>/users")(get_company_users)
 bp.post("")(create_company)
 bp.post("/login")(login)
