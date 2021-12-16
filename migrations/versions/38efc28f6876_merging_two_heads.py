cc4385ce403bca8def93f240a050170ee5003873
kelvin alisson cantarino <kelvininfortec@gmail.com>
2021-12-16 11:46:29 +0000

    Revert "Merge branch 'feat/new_doc' into 'developer'"
    This reverts merge request !54

---
+++ migrations/versions/38efc28f6876_merging_two_heads.py
@@ -0,0 +1,24 @@
+"""merging two heads
+
+Revision ID: 38efc28f6876
+Revises: cb16fc2136a3, 78ce714c1725
+Create Date: 2021-12-13 15:22:35.531666
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '38efc28f6876'
+down_revision = ('cb16fc2136a3', '78ce714c1725')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ migrations/versions/d968fc03d864_merging_two_heads.py
@@ -0,0 +1,24 @@
+"""merging two heads
+
+Revision ID: d968fc03d864
+Revises: 38efc28f6876, 9b31730becee
+Create Date: 2021-12-13 18:07:28.251862
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd968fc03d864'
+down_revision = ('38efc28f6876', '9b31730becee')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
